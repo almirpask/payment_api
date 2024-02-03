5b29d0259410e65c0bbfdbf8bb00580bd342d578
almirpask <almirpask@hotmail.com>
2024-02-03 18:42:50 -0300

    create rabbitmq pkg

---
+++ pkg/rabbitmq/rabbitmq.go
@@ -0,0 +1,57 @@
+package rabbitmq
+
+import (
+	"context"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func OpenChannel() (*amqp.Channel, error) {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+
+	if err != nil {
+		panic(err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+
+	return ch, nil
+}
+
+func Consume(ch *amqp.Channel, out chan amqp.Delivery, queue string) error {
+	msgs, err := ch.Consume(
+		queue,
+		"go-payment",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	for msg := range msgs {
+		out <- msg
+	}
+
+	return nil
+}
+
+func Publish(ctx context.Context, ch *amqp.Channel, body, exchange string) error {
+	err := ch.PublishWithContext(ctx, exchange, "", false, false, amqp.Publishing{
+		ContentType: "text/json",
+		Body:        []byte(body),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
